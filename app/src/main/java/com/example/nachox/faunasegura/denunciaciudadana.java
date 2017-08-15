ae681ac05621126a2d3990cb1f175391bb353440
Arciga <nacho_arciga2@hotmail.com>
2017-08-15 15:17:52 -0500

    actualizacion

--- app/src/main/java/com/example/nachox/faunasegura/denunciaciudadana.java
+++ app/src/main/java/com/example/nachox/faunasegura/denunciaciudadana.java
@@ -13,11 +13,6 @@ public class denunciaciudadana extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.denunciaciudadana);
 
-        WebView view = (WebView) findViewById(R.id.webView);
-        view.getSettings().setJavaScriptEnabled(true);
-        view.getSettings().setBuiltInZoomControls(true);
-        browser.getSettings().setDisplayZoomControls(false);
-
         browser=(WebView)findViewById(R.id.webView);
         browser.setWebViewClient(new WebViewClient() {
             @Override public boolean shouldOverrideUrlLoading(WebView view, String url) {
