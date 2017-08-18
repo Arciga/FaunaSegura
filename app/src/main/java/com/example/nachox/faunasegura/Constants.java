50752bf9b6a0087f61ee5bd110c8cb1651b2f1f2
Arciga <nacho_arciga2@hotmail.com>
2017-08-17 21:03:56 -0500

    Proyecto Actualizado Respecto a la Base de Datos

--- app/src/main/java/com/example/nachox/faunasegura/Constants.java
+++ app/src/main/java/com/example/nachox/faunasegura/Constants.java
@@ -4,6 +4,6 @@ package com.example.nachox.faunasegura;
  * Created by Belal on 6/10/2016.
  */
 public class Constants {
-    public static final String UPLOAD_URL = "http://104.198.61.117/AndroidImageUpload/upload.php";
-    public static final String IMAGES_URL = "http://104.198.61.117/AndroidImageUpload/getImages.php";
+    public static final String UPLOAD_URL = "http://104.198.61.117/FaunaSeguraProyect/Uploads/Mascotas/upload.php";
+    public static final String IMAGES_URL = "http://104.198.61.117/FaunaSeguraProyect/Uploads/Mascotas/getImages.php";
 }
